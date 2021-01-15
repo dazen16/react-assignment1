b9018fb6d430f3c00084bd933e805cc39fd4da38
dazen16 <dzeng10@hotmail.com>
2021-01-15 15:34:41 -0800

    changed response

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return axios.post('http://localhost:5000/users', character)
       .then(function (response) {
         console.log(response);
-        return (response.status === 201);
+        return (response);
       })
       .catch(function (error) {
         console.log(error);
@@ -71,8 +71,8 @@ class App extends Component {
   }
   handleSubmit = character => {
     this.makePostCall(character).then( callResult => {
-      if (callResult === true) {
-        this.setState({ characters: [...this.state.characters, character] });
+      if (callResult.status === 201) {
+        this.setState({ characters: [...this.state.characters, callResult.data] });
       }
     });
   }
